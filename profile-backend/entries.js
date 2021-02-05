6983e868e9dd3b114523ff9de7be7c5eacdb3756
Ranjeet Kumar <nranjeet4ne@gmail.com>
2021-02-06 02:47:18 +0530

    Web Profile

---
+++ profile-backend/entries.js
@@ -0,0 +1,20 @@
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('profile-database.json')
+const db = low(adapter)
+db.defaults({ entries: [] }).write()
+
+function putEntry(entry) {
+    db.get('entries').push(entry).write()
+// TODO: store it locally
+}
+
+function getEntries() {
+    return db.get('entries').value()
+}
+
+module.exports = {
+    putEntry,
+    getEntries,
+}
---
+++ profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "RANJEET KUMAR",
+  greeting: "Hey!👋, I'm Glad you're here :)",
+  greetingDescription: "I'm RANJEET and I'm a Software Engineer by passion!",
+  githubUrl: "https://github.com/nranjeet",
+  linkedinUrl: "https://www.linkedin.com/in/ranjeet-kumar-8a0022147/",
+  cvLink: "https://drive.google.com/file/d/1vovLy_4U3BRQm2H3Ox0BMqfDw31NLl4c/view?usp=sharing",
+};
